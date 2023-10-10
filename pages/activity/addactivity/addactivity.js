b401d217794ab448a502af38317149a1c35824a5
Mastore84 <Mpb.petersen@gmail.com>
2023-10-10 12:21:03 +0200

    Added to addactivity (unfinished)

--- pages/activity/addactivity/addactivity.js
+++ pages/activity/addactivity/addactivity.js
@@ -1,8 +1,26 @@
-
-import { API_URL,FETCH_NO_API_ERROR } from "../../../settings.js"
+import { API_URL, handleHttpErrors, makeOptions } from "../../../settings.js";
 //Add id to this URL to get a single user
-import { makeOptions, handleHttpErrors} from "../../../utils.js"
+import { makeOptions, handleHttpErrors } from "../../../utils.js";
 
 export async function initAddActivity() {
-
-   }
+  document
+    .getElementById("activity-form")
+    .addEventListener("submit", async (e) => {
+      e.preventDefault();
+      const form = e.target;
+      const name = form.name.value;
+      const price = form.price.value;
+      const minAge = form.minAge.value;
+      const capacity = form.capacity.value;
+      const activity = {
+        name,
+        price,
+        minAge,
+        capacity,
+      };
+      const options = makeOptions("POST", activity);
+      const res = await fetch(`${API_URL}/activity`, options).then(
+        handleHttpErrors
+      );
+    });
+}
