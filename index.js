102eab374f8b2c1210fbd1418eb1c40e45d33e2b
Jonathan Finner Smith <jona08p8@stud.kea.dk>
2023-10-10 12:08:52 +0200

    tilføjet ændring til home

--- index.js
+++ index.js
@@ -103,9 +103,8 @@ window.addEventListener("load", async () => {
       //For very simple "templates", you can just insert your HTML directly like below
       "/": () => document.getElementById("content").innerHTML = `
         <h2>Home</h2>
-        <img style="width:50%;max-width:600px;margin-top:1em;" src="./images/cars.png">
         <p style='margin-top:1em;font-size: 1.5em;color:darkgray;'>
-          Car's 'R' Us - Created, as a help to make GREAT fullstack developers <span style='font-size:2em;'>&#128516;</span>
+          Adventure - Created  as a help to make GREAT fullstack developers <span style='font-size:2em;'>&#128516;</span>
         </p>
      `,
      //Customer
