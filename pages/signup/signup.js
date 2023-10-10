f262107f2b42d25d95174603535f26e346cd1c0b
Jonathan Finner Smith <jona08p8@stud.kea.dk>
2023-10-10 10:42:23 +0200

    added ting til login

--- pages/signup/signup.js
+++ pages/signup/signup.js
@@ -1,10 +1,10 @@
-import { API_URL} from "../../../settings.js"
-import { makeOptionsToken, handleHttpErrors} from "../../../utils.js"
+import { API_URL} from "../../settings.js"
+import { makeOptionsToken, makeOptions, handleHttpErrors} from "../../utils.js"
 
-const URL = API_URL + "/members"
+const URL = API_URL + "/customer"
 
 export function initSignup() {
-    document.getElementById("new-member-response").innerText = "";
+    document.getElementById("new-customer-response").innerText = "";
     document.getElementById("form").addEventListener("submit", singUp)
 }
 
@@ -12,24 +12,23 @@ export function initSignup() {
 async function singUp(event) {
     event.preventDefault();
   var username = document.getElementById("input-username").value
-  var email = document.getElementById("input-email").value
   var password = document.getElementById("input-password").value
+  var email = document.getElementById("input-email").value
   var firstName = document.getElementById("input-firstname").value
   var lastName = document.getElementById("input-lastname").value
-  var street = document.getElementById("input-street").value
-  var city = document.getElementById("input-city").value
-  var zip = document.getElementById("input-zip").value
+  var phoneNumber = document.getElementById("input-phone").value
+  var address = document.getElementById("input-address").value
+  
 
-  const member = { username, email, password, firstName, lastName, street, city, zip }
+  const customer = { username, password, email, firstName, lastName, phoneNumber, address }
 
-  const options = makeOptionsToken("POST", member, true);
+  const options = makeOptions("POST", customer);
   fetch(URL, options)
   .then(handleHttpErrors)
   .then( 
-    carResponse => document.getElementById("new-member-response")
+    carResponse => document.getElementById("new-customer-response")
     .innerText = JSON.stringify(carResponse, null, 3)).catch(err =>
-        document.getElementById("new-member-response").innerHTML = err
+        document.getElementById("new-customer-response").innerHTML = err
     )
-
-    document.getElementById("input-username").reset();
+    
 }
