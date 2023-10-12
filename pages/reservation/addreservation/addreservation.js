1cc5132f1bc275bc93c854acb76abffd97330b72
Jonathan Finner Smith <jona08p8@stud.kea.dk>
2023-10-12 21:23:14 +0200

    har lavet lidt i add reservation

--- pages/reservation/addreservation/addreservation.js
+++ pages/reservation/addreservation/addreservation.js
@@ -2,20 +2,59 @@
 import { API_URL,FETCH_NO_API_ERROR } from "../../../settings.js"
 //Add id to this URL to get a single user
 import { makeOptionsToken, handleHttpErrors} from "../../../utils.js"
+const URL = `${API_URL}/reservation`;
 
 export async function initAddReservation() {
 
-  // Assuming this needs to be a POST request
-  const requestOptions = makeOptionsToken("POST", { name: document.getElementById("name").value, date: document.getElementById("date").value, time: document.getElementById("time").value }, true);
+    document.getElementById("btn-fetch-activity").addEventListener("click", findActivity);
+    document.getElementById("btn-submit-reservation").addEventListener("click", addReservation);   
   
-  fetch(API_URL+"reservations", requestOptions)
+}
+
+async function findActivity() {
+  
+      document.getElementById("succes").innerText = " ";
+      document.getElementById("error").innerText = " ";
+  
+      try {
+  
+          const form = document.getElementById("addReservationForm");
+  
+          const idActivity = document.getElementById("activity-id-input").value;
+          const options = makeOptionsToken("GET", null, true);
+          const responseActivity = await fetch(`${API_URL}/activity/${idActivity}`, options);
+          if (!responseActivity.ok) {
+              throw new Error(`Error fetching activity with ID ${idActivity}: ${responseActivity.statusText}`);
+          }
+          const activity = await responseActivity.json();
+          form.activityId.value = activity.id;
+  
+      } catch (error) {
+          console.error("Failed to fetch and populate the details:", error);
+          document.getElementById("error").innerText = "activity with that ID does not exist";
+      }
+}
+
+async function addReservation() {
+
+    document.getElementById("succes").innerText = " ";
+    document.getElementById("error").innerText = " ";
+
+    const reservation = {
+        activityId: document.getElementById("activity-id-input").value,
+        reservationStart: document.getElementById("start-time").value,
+        reservationEnd: document.getElementById("end-time").value,
+        participants: document.getElementById("Participants").value,
+    };
+
+    const options = makeOptionsToken("POST", reservation, true);
+    fetch(URL, options)
     .then(handleHttpErrors)
-    .then((createdReservation)=> {
-        // Handle the created reservation... Redirect, show message, etc.
-        console.log(createdReservation)
-    })
-    .catch((error) => {
-         // Handle error
-         console.error(FETCH_NO_API_ERROR, error);
-    });
+     .then( 
+       reservationResponse => document.getElementById("new-reservation-response")
+       .innerText = JSON.stringify(reservationResponse, null, 3)
+       )
+       .catch(err => document.getElementById("error").innerText = err) 
+       document.getElementById("addReservationForm").reset();
+
 }
